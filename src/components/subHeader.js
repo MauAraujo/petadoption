f81537bdbc91e35eccfe2d82d6e3375237006a6f
thomasnrggo <thomasnrggo@gmail.com>
2020-03-12 02:05:00 -0600

    catalog without filters

--- src/components/subHeader.js
+++ src/components/subHeader.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import "./styles/NavBar.scss"
+import './styles/sub-header.scss'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCalendarAlt, faHeart, faUser } from "@fortawesome/free-regular-svg-icons"
 import { faPaw } from '@fortawesome/free-solid-svg-icons'
@@ -8,17 +9,17 @@ let mascota = "Gatos"
 export default function SubHeader() {
     return (
         <Fragment>
-            <div className="container-fluid sub-header my-1">
-                <div className="row al">
-                    <div className="col-6">
+            <div className="container-fluid sub-header">
+                <div className="row pt-4 pb-2">
+                    <div className="col-12 col-md-1">
                         <FontAwesomeIcon icon={faPaw} size='2x' />
-                        <h3>{mascota}</h3>
+                        <span className='ml-2'>{mascota}</span>
                     </div>
-                    <div className="col-6">
-                        <div className="input-group mb-3">
-                            <input type="text" className="form-control search" placeholder="Ingrese código postal" aria-label="Ingrese código postal" aria-describedby="Código postal" />
+                    <div className="col-12 col-md-3 offset-md-8">
+                        <div className="input-group">
+                            <input type="text" className="form-control search input-outline" placeholder="Ingrese código postal" aria-label="Ingrese código postal" aria-describedby="Código postal" />
                             <div className="input-group-append">
-                                <button className="btn btn-outline search " type="button" id="button-addon2">Button</button>
+                                <button className="btn btn-outline search " type="button" id="button-addon2">Buscar</button>
                             </div>
                         </div>
                     </div>
