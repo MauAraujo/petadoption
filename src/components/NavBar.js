4770a758e4eb21b6afb7f2bfcd8806d6ba7a6193
Noe Osorio <noe.osorio@smartkode.mx>
2020-04-08 01:09:03 -0500

    cambio a componentes bootstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,17 +3,68 @@ import { NavLink } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBars } from "@fortawesome/free-solid-svg-icons"
 import './styles/NavBar.scss'
+import { Navbar, Container, Nav, Image, Row, Col } from 'react-bootstrap'
+import logo from '../assets/images/cat.svg'
+
 
 export default function NavBar() {
   return (
-    <nav className='navbar navbar-expand-lg navbar-light fixed-top'>
-      <div className='container'>
-        <NavLink className='navbar-brand d-none d-sm-block' to='/'>
-          Logotipo
-          {/* <img className='img-fluid logo' src={logo} alt='' /> */}
-        </NavLink>
+    <Navbar collapseOnSelect expand="lg" fixed="top" >
+      <Container>
+        <Navbar.Brand fluid>
 
-        <button
+          <NavLink className='navbar-brand d-none d-sm-block' to='/'>
+            Logotipo
+            {/* <Image className="logo"  src={logo}></Image> */}
+            {/* 
+              <Row>
+                <Col sm={6} lg={2}>
+                  
+                </Col>
+                <Col >
+                Logotipo
+                </Col>
+              </Row> */}
+
+
+            {/* <img className='img-fluid logo' src={logo} alt='' /> */}
+          </NavLink>
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="header" />
+        <Navbar.Collapse id="header">
+          <Nav className='mr-auto'>
+            <Nav.Item >
+              <span>|</span>
+            </Nav.Item>
+            <Nav.Item className='nav-item-capital'>
+              <NavLink exact activeClassName='active' to='/catalogo'>
+                Adopciones
+              </NavLink>
+            </Nav.Item>
+          </Nav>
+          <Nav className='ml-auto'>
+            <Nav.Item className='nav-item-capital'>
+              <NavLink exact activeClassName='active' to='/catalogo'>
+                Adopta ahora
+              </NavLink>
+            </Nav.Item>
+            <Nav.Item >
+              <span>|</span>
+            </Nav.Item>
+            <Nav.Item >
+              <NavLink exact activeClassName='active' to='/'>
+                Ingresar
+              </NavLink>
+            </Nav.Item>
+            <Nav.Item >
+              <NavLink exact activeClassName='active' to='/'>
+                <FontAwesomeIcon icon={faBars} />
+              </NavLink>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+        {/* <button
           className='navbar-toggler'
           type='button'
           data-toggle='collapse'
@@ -22,41 +73,14 @@ export default function NavBar() {
           aria-expanded='false'
           aria-label='Toggle navigation'>
           <span className='navbar-toggler-icon'></span>
-        </button>
+        </button> */}
 
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul className='navbar-nav mr-auto'>
-            <li className='nav-item'>
-              <span>|</span>
-            </li>
-            <li className='nav-item-capital'>
-              <NavLink exact activeClassName='active' to='/catalogo'>
-                Adopciones
-              </NavLink>
-            </li>
-          </ul>
-          <ul className='navbar-nav ml-auto'>
-            <li className='nav-item-capital'>
-              <NavLink exact activeClassName='active' to='/catalogo'>
-                Adopta ahora
-              </NavLink>
-            </li>
-            <li className='nav-item'>
-              <span>|</span>
-            </li>
-            <li className='nav-item'>
-              <NavLink exact activeClassName='active' to='/'>
-                Ingresar
-              </NavLink>
-            </li>
-            <li className='nav-item'>
-              <NavLink exact activeClassName='active' to='/'>
-                <FontAwesomeIcon icon={faBars} />
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+
+      </Container>
+
+    </Navbar>
+    // <nav className='navbar navbar-expand-lg navbar-light fixed-top'>
+
+    // </nav>
   )
 }
