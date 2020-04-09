6b2d2bd15a8f73d7036d4c310b7d91750279aef3
Noe Osorio <noe.osorio@smartkode.mx>
2020-04-08 21:08:31 -0500

    Minor Fix

--- src/pages/catalogo.js
+++ src/pages/catalogo.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import './styles/catalogo.scss'
 //components
 import SubHeader from '../components/subHeader'
+import { Col, Container, Row } from 'react-bootstrap'
 
 let dummy = 'https://i.pinimg.com/originals/22/d2/aa/22d2aa3cf43c1e6a72d18887be3846c2.jpg'
 
@@ -9,14 +10,14 @@ export default function Catalogo() {
 
   let card = () => {
     return (
-      <div className='col-12 col-md-2 mb-5'>
+      <Col md={2}  className="mb-5">
         <div className='img-container'>
           <img className='contain' src={dummy} alt={dummy} />
         </div>
         <div className='title-container'>
           <h6 className='subtitle-pet text-center'>Nombre</h6>
         </div>
-      </div>
+      </Col>
     )
   }
 
@@ -24,13 +25,12 @@ export default function Catalogo() {
     <Fragment>
       <SubHeader />
       <section className='bg-yellow'>
-        <div className='container-fluid'>
-          <div className='row py-4'>
-            <div className='col-12 col-md-2'>aside</div>
-            {/* // area de catalogo */}
-            <div className='col-12 col-md-8'>
+        <Container fluid>
+          <Row className="py-4">
+            <Col md={2} >Aside</Col>
+            <Col md={8} >
               <h2 className='subtitle'>Mascotas</h2>
-              <div className='row catalogo'>
+              <Row className="catalogo">
                 {card()}
                 {card()}
                 {card()}
@@ -47,35 +47,35 @@ export default function Catalogo() {
                 {card()}
                 {card()}
                 {card()}
-              </div>
-            </div>
-          </div>
-        </div>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
       </section>
     </Fragment>
   )
 }
 
-export function Select() {
-  return (<Fragment>
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          Raza
-        </div>
-        <div className="col-12">
-          <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown button
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </Fragment>)
-}
+// export function Select() {
+//   return (<Fragment>
+//     <div className="container">
+//       <div className="row">
+//         <div className="col-12">
+//           Raza
+//         </div>
+//         <div className="col-12">
+//           <div class="dropdown">
+//             <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//               Dropdown button
+//             </button>
+//             <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+//               <a class="dropdown-item" href="#">Action</a>
+//               <a class="dropdown-item" href="#">Another action</a>
+//               <a class="dropdown-item" href="#">Something else here</a>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   </Fragment>)
+// }
