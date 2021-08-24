76d5d35c2faa5e1f02360ef0d6029910d178af2d
Mauricio Araujo <mauricio.araujo@protonmail.com>
2021-08-24 18:54:37 -0500

    Add chatbot interface

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -5,6 +5,7 @@ import SearchBar from "../components/search-bar";
 import Available from "../components/available";
 import Steps from "../components/steps";
 import DiscoverCard from "../components/discover-card";
+import Widget from 'rasa-webchat';
 import "../services/publications.service";
 import { getPublications } from "../services/publications.service";
 import { useHistory } from "react-router-dom";
@@ -34,6 +35,16 @@ export default function Homepage() {
       <div className="discovery-container discovery">
         <DiscoverCard />
       </div>
+      <Widget
+        initPayload={"/utter_greet"}
+        socketUrl={"http://localhost:5005"}
+        socketPath={"/socket.io/"}
+        customData={{ "language": "es" }} // arbitrary custom data. Stay minimal as this will be added to the socket
+        title={"Ayuda"}
+        inputTextFieldHint={'Escribe un mensaje'}
+        showFullScreenButton={true}
+        displayUnreadCount={true}
+      />
     </div>
   );
 }
