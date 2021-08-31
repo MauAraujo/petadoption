d566fdb8b3cde5f330e7f6ede66d49bd03cc0e1c
Mauricio Araujo <mauricio.araujo@protonmail.com>
2021-08-30 19:13:15 -0500

    Add custom action

--- assistant/actions/actions.py
+++ assistant/actions/actions.py
@@ -7,21 +7,51 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+import dog_breeds
+import requests
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class Action_Breed_Api(Action):
+
+    def name(self) -> Text:
+        return "action_breed_api"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        animal = tracker.get_slot("animal")
+        age = tracker.get_slot("age")
+        breed = tracker.get_slot("breed")
+
+        image = retrieve_breed_image(breed)
+        recommendations = retrieve_age_recommendations(age)
+
+        return []
+
+
+def retrieve_breed_image(breed):
+    split_breed = breed.split()
+    dog_breeds_set = set(dog_breeds.all)
+
+    if (len(split_breed) > 1) and (split_breed[0] in dog_breeds_set):
+        query = f'{split_breed[0]}/{split_breed[1]}'
+    elif len(split_breed) > 1:
+        query = f'{split_breed[1]}/{split_breed[0]}'
+    else:
+        query = breed
+
+    if animal == "perro":
+        uri = f'https://dog.ceo/api/breed/{query}/images/random'
+    else:
+        uri = f''
+    r = requests.get(uri)
+
+    return r.json()["message"]
+
+
+def retrieve_age_recommendations(age):
+    pass
